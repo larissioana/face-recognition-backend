61c758df6e467bbed1e6017a48aad14e86482bcc
Larisa Ioana <larisaoltean27@outlook.com>
2023-03-02 12:07:03 +0200

    some changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const register=require('./controllers/register');
 const signin=require('./controllers/signin');
 const profile=require('./controllers/profile');
 const image=require('./controllers/image');
-
+DATABASE_URL='postgres://ggkcmcgpuyoumc:04030a1a8490aa9f03235740d1b95ce057cb10cdbdb15dd889162a8a198fce10@ec2-52-54-212-232.compute-1.amazonaws.com:5432/d44ph7k2q59adn'
 
 const db=knex({
     client: 'pg',
@@ -17,7 +17,7 @@ const db=knex({
      }
     }
   });
-
+console.log(DATABASE_URL)
 const app=express();
 app.use(cors());
 app.use(express.json());
