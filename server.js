84fb1000d737e4c4255e7d71f61f42b5d1ef5a46
Larisa Ioana <larisaoltean27@outlook.com>
2023-03-02 09:20:30 +0200

    port changed

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get('/profile/:id', (req,res)=>{profile.handleProfile(req,res,db)});
 app.put('/image', (req,res)=>{image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res)=>{image.handleApiCall(req,res)});
 
-app.listen(3000, ()=>{
-    console.log('app is running on port 3000')
+const PORT=process.env.PORT || 3030;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`)
   });
