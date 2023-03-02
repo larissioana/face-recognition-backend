bd2857c8e1e74494630a6d7162b38c36de947822
Larisa Ioana <larisaoltean27@outlook.com>
2023-03-02 11:20:49 +0200

    adding database connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image=require('./controllers/image');
 const db=knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-sinuous-07294',
-      user : 'larisaoltean',
-      password:'',
-      database : 'smart-app'
+     host:process.env.DATABASE_URL,
+     ssl:true,
     }
   });
 
